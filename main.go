27a33146a31dee8451671d79313575fc8dc475b1
evan <gundamhina@hotmail.com>
2017-11-25 15:04:52 +0800

    1.加入除法並測試

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
     "net/http"
-    "strconv"
 
     "github.com/labstack/echo"
+    "strconv"
+    "errors"
 )
 
 func main() {
@@ -27,9 +28,35 @@ func main() {
         result := Add(num1, num2)
         return c.String(http.StatusOK, strconv.Itoa(result))
     })
-    e.Logger.Fatal(e.Start(":1323"))
+
+    e.GET("/div/:num1/:num2", func(c echo.Context) error {
+        num1Str := c.Param("num1")
+        num2Str := c.Param("num2")
+        num1, err := strconv.Atoi(num1Str)
+        if err != nil {
+            return c.String(http.StatusUnprocessableEntity, err.Error())
+        }
+        num2, err := strconv.Atoi(num2Str)
+        if err != nil {
+            return c.String(http.StatusUnprocessableEntity, err.Error())
+        }
+        result, err := Div(num1, num2)
+        if err != nil {
+            return c.String(http.StatusUnprocessableEntity, err.Error())
+        }
+        return c.String(http.StatusOK, strconv.Itoa(result))
+    })
+
+    e.Logger.Fatal(e.Start(":1325"))
 }
 
 func Add(num1, num2 int) int {
     return num1 + num2
+}
+
+func Div(num1, num2 int) (int, error) {
+    if num2 == 0{
+        return 0, errors.New("number 2 cnannot be 0")
+    }
+    return num1 / num2, nil
 }
--- main_test.go
+++ main_test.go
@@ -10,3 +10,20 @@ func TestAdd(t *testing.T) {
 		t.Errorf("should be 4 but %d", result)
 	}
 }
+
+func TestDiv(t *testing.T) {
+	result,err := Div(4, 2)
+	if err!=nil{
+		t.Error(err.Error())
+	}
+	if result != 2 {
+		t.Errorf("should be 4 but %d", result)
+	}
+}
+
+func TestDivZero(t *testing.T) {
+	_, err := Div(4, 0)
+	if err==nil{
+		t.Error("cannot pass")
+	}
+}
